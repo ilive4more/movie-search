8353d07641d0d5d27793a620674bc5d26f502c82
James Adams <jimmyadams61@gmail.com>
2019-09-08 18:06:14 -0700

    Added Asyc/await and eerror message when no result returned

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
 // movie search controller
-document.getElementById("submit").addEventListener("click", function(e) {
+document.getElementById('submit').addEventListener('click', function(e) {
   e.preventDefault();
   movieSearch();
 });
 
-function showobj() {
-  document.getElementById("title").innerText = obj.Title;
-  document.getElementById("actors").innerText = obj.Actors;
-  document.getElementById("plot").innerText = obj.Plot;
-  document.getElementById("release").innerText = obj.Released;
-  document.querySelector("img").src = obj.Poster;
-}
+let movieSearch = async title => {
+  title = document.getElementById('data').value;
 
-let obj = {};
-
-function movieSearch(title) {
-  title = document.getElementById("data").value;
-  console.log(title);
-  fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=e8cff417&t=${title}`)
-    .then(res => res.json())
-    .then(data => (obj = data))
-    .then(obj => console.log(obj))
-    .then(function() {
-      showobj();
-    });
-}
+  let response = await fetch(
+    `http://www.omdbapi.com/?i=tt3896198&apikey=e8cff417&t=${title}`
+  );
+  // console.log(response);
+  let data = await response.json();
+  if (data.Title === undefined) {
+    alert('No results found.  Please try again!');
+  } else {
+    document.getElementById('title').innerText = data.Title;
+    document.getElementById('actors').innerText = data.Actors;
+    document.getElementById('plot').innerText = data.Plot;
+    document.getElementById('release').innerText = data.Released;
+    document.querySelector('img').src = data.Poster;
+  }
+};
